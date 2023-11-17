669f7f31c101e4b79740387c8c3368482342c308
SergioRon2 <rondonjr2005@gmail.com>
2023-11-17 02:12:15 -0500

    17/11/2023

--- app/usuarios/page.tsx
+++ app/usuarios/page.tsx
@@ -1,20 +1,40 @@
+'use client';
+import { apiRestGet } from '@/services/services'
+import { useEffect, useState } from 'react'
 import StyleUsuarios from 'styles/usuarios.module.css'
 
 
+
 export default function Miembros(){
+
+    const [usuarios, setUsuarios] = useState([])
+
+    useEffect(()=>{
+        const datosUsuarios = async() => {
+            const res = await apiRestGet('plan');
+            setUsuarios(res.tarjetas);
+            console.log(res.tarjetas)
+        }
+
+        datosUsuarios()
+    }, [])
+
     return <>
         <div className={StyleUsuarios.container}>
-            <div className={StyleUsuarios.card}>
-                <h2>Nombre: Fulano De Tal </h2>
-                <h4>ID: 1000222002</h4>
-                <h4>Plan: 30 dias</h4>
-                <br />
-                <div className={StyleUsuarios.buttons}>
-                    <button className={StyleUsuarios.buttonBlue}>Ver</button>
-                    <button className={StyleUsuarios.buttonGreen}>Actualizar</button>
-                    <button className={StyleUsuarios.buttonRed}>Eliminar</button>   
-                </div>
-            </div>
+            {
+                (usuarios ?? [])?.map((user:any, index:any) => (
+                    <div className={StyleUsuarios.card}>
+                        <h2>{user.usuario.nombre} {user.usuario.apellido}</h2>
+                        <h4>ID: {user.usuario.id}</h4>
+                        <h4>Dias restantes: {user.dias_restantes}</h4>
+                        <div className={StyleUsuarios.buttons}>
+                            <button className={StyleUsuarios.buttonBlue}>Ver</button>
+                            <button className={StyleUsuarios.buttonGreen}>Actualizar</button>
+                            <button className={StyleUsuarios.buttonRed}>Eliminar</button>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     </>
 }
---
+++ services/services.ts
@@ -0,0 +1,90 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/gymapp/'
+
+async function getRequestHeaders() {
+  try {
+    const headers = {
+      Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+      session: localStorage.getItem('access'),
+    };
+
+    return headers;
+  } catch (error) {
+    console.error('Error al obtener encabezados:', error);
+    return {};
+  }
+}
+
+export async function apiRestGet(path: string, body = {}): Promise<any> {
+  try {
+    const requestOptions: any = {}
+
+    requestOptions.headers = await getRequestHeaders()
+    requestOptions.params = body
+    const response = await axios.get(path, requestOptions)
+    return response.data
+  } catch (e) {
+    return e
+  }
+}
+
+export async function apiRestDelete(path: string, body = {}): Promise<any> {
+  try {
+    const requestOptions: any = {}
+    requestOptions.headers = await getRequestHeaders()
+
+    requestOptions.params = body
+    const response = await axios.delete(path, requestOptions)
+    return response.data
+  } catch (e) {
+    return e
+  }
+}
+
+export async function apiRestPost(path: string, body: any = {}): Promise<any> {
+  try {
+    const requestOptions: any = {}
+    requestOptions.headers = await getRequestHeaders()
+    const response = await axios.post(path, body, requestOptions)
+    return response.data
+  } catch (e) {
+    return { error: e }
+  }
+}
+
+export async function apiRestPatch(
+  path: string,
+  body: any = {},
+  headers?: any
+): Promise<any> {
+  try {
+    const requestOptions: any = {}
+    requestOptions.headers = await getRequestHeaders()
+
+    if (headers) {
+      requestOptions.headers = {
+        ...requestOptions.headers,
+        ...headers,
+      }
+    }
+
+    const response = await axios.patch(path, body, requestOptions)
+
+    return response.data
+  } catch (e) {
+    return { error: e }
+  }
+}
+
+export async function apiRestPut(path: string, body: any = {}): Promise<any> {
+  try {
+    const requestOptions: any = {}
+    requestOptions.headers = await getRequestHeaders()
+
+    const response = await axios.put(path, body, requestOptions)
+    return response.data
+  } catch (e) {
+    return { error: e }
+  }
+}
