295a43eba9b55dc6d9eb57ada776d9510455f266
SergioRon2 <rondonjr2005@gmail.com>
2023-11-17 18:50:54 -0500

    17/11/2023

--- app/usuarios/page.tsx
+++ app/usuarios/page.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { apiRestGet } from '@/services/services'
+import { apiRestDelete, apiRestGet } from '@/services/services'
 import { useEffect, useState } from 'react'
 import StyleUsuarios from 'styles/usuarios.module.css'
+import swal from 'sweetalert'
 
 
 
@@ -13,24 +14,65 @@ export default function Miembros(){
         const datosUsuarios = async() => {
             const res = await apiRestGet('plan');
             setUsuarios(res.tarjetas);
-            console.log(res.tarjetas)
+            console.log(res.tarjetas[0])
         }
 
         datosUsuarios()
     }, [])
 
+
+// Función para manejar la eliminación del usuario
+const eliminarUsuario = async (userId: number) => {
+  // Mostrar una SweetAlert de confirmación
+  const confirmDelete = await swal({
+    title: '¿Estás seguro?',
+    text: 'Esta acción no se puede deshacer',
+    icon: 'warning',
+    buttons: ['Cancelar', 'Sí, eliminar'],
+    dangerMode: true,
+  });
+
+  // Verificar si el usuario confirmó la eliminación
+  if (confirmDelete) {
+    try {
+      // Realizar la solicitud DELETE a la API
+      const response = await apiRestDelete(`eliminar/${userId}`);
+      console.log(response)
+
+      // Verificar si la eliminación fue exitosa
+      if (response.success) {
+        // La eliminación fue exitosa, mostrar SweetAlert de éxito
+        swal('Éxito', response.mensaje, 'success');
+        // Puedes realizar otras acciones después de la eliminación si es necesario
+      } else {
+        // La eliminación falló, mostrar SweetAlert de error
+        swal('Error', response.mensaje, 'error');
+      }
+    } catch (error) {
+      // Manejar errores de la solicitud DELETE, si es necesario
+      console.error('Error al eliminar el usuario:', error);
+      swal('Error', 'Error al eliminar el usuario', 'error');
+    }
+  } else {
+    // El usuario canceló la eliminación, puedes mostrar un mensaje opcional
+    swal('Cancelado', 'La eliminación ha sido cancelada', 'info');
+  }
+};
+
+
+
     return <>
         <div className={StyleUsuarios.container}>
             {
-                (usuarios ?? [])?.map((user:any, index:any) => (
+                usuarios.map((user:any, index:any) => (
                     <div className={StyleUsuarios.card}>
                         <h2>{user.usuario.nombre} {user.usuario.apellido}</h2>
-                        <h4>ID: {user.usuario.id}</h4>
+                        <h4>ID: {user.usuario.id_usuario}</h4>
                         <h4>Dias restantes: {user.dias_restantes}</h4>
                         <div className={StyleUsuarios.buttons}>
                             <button className={StyleUsuarios.buttonBlue}>Ver</button>
                             <button className={StyleUsuarios.buttonGreen}>Actualizar</button>
-                            <button className={StyleUsuarios.buttonRed}>Eliminar</button>
+                            <button onClick={()=>{eliminarUsuario(user.usuario.id)}} className={StyleUsuarios.buttonRed}>Eliminar</button>
                         </div>
                     </div>
                 ))
