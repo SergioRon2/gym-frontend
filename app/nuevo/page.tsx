244c769ba45aa6723b5af468c1f81e0e61c20000
SergioRon2 <rondonjr2005@gmail.com>
2023-11-19 02:31:01 -0500

    19/11/2023

--- app/nuevo/page.tsx
+++ app/nuevo/page.tsx
@@ -8,28 +8,38 @@ export default function MiembroNuevo() {
       nombre: '',
       apellido: '',
       tipo_id: '',
-      id_usuario: 0,
+      id_usuario: '',
       plan: '',
       fechaInicio: '',
     });
   
     const handleChange = (e:any) => {
+      const {name, value} = e.target;
       setFormData({
         ...formData,
-        [e.target.name]: e.target.value,
+        [name]: value,
       });
     };
   
     const handleSubmit = async (e:any) => {
       e.preventDefault();
-  
       try {
         const response = await apiRestPost('/nuevo_usuario/', formData);
-        console.log(response);
+        if (response.success) {
+          // Aquí manejas el caso de éxito
+          console.log(response.usuario)
+        } else {
+          // Aquí manejas el caso de error
+          console.log('Error al registrar nuevo miembro:', response.errors)
+          // Puedes mostrar mensajes de error específicos para cada campo si lo deseas
         // Aquí puedes manejar la respuesta, redireccionar, mostrar mensajes, etc.
+        }
       } catch (error) {
         console.error('Error al registrar nuevo miembro:', error);
+        console.log(error)
       }
+
+      console.log(formData)
     };
   
     return (
@@ -39,40 +49,44 @@ export default function MiembroNuevo() {
           <form onSubmit={handleSubmit} className={NuevoStyle.formulario}>
             <div className={NuevoStyle.inputs}>
               <label htmlFor="nombre">Nombre(s):</label>
-              <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
+              <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} required/>
             </div>
             <div className={NuevoStyle.inputs}>
               <label htmlFor="apellido">Apellido(s):</label>
-              <input type="text" name="apellido" value={formData.apellido} onChange={handleChange}/>
+              <input type="text" name="apellido" value={formData.apellido} onChange={handleChange} required/>
             </div>
             <div className={NuevoStyle.select}>
               <label htmlFor="tipo_id">Identificacion:</label>
-              <select name="tipo_id" value={formData.tipo_id} onChange={handleChange}>
-                <option value="">Tarjeta de identidad</option>
-                <option value="">Cedula de ciudadania</option>
-                <option value="">Cedula de extranjeria</option>
-                <option value="">Pasaporte</option>
-                <option value="">Permiso especial de Permanencia</option>
+              <select name="tipo_id" value={formData.tipo_id} onChange={handleChange} required>
+                <option value="">Seleccione el tipo de Identificacion</option>
+                <option value="ti">Tarjeta de identidad</option>
+                <option value="cedula">Cedula de ciudadania</option>
+                <option value="ce">Cedula de extranjeria</option>
+                <option value="pasaporte">Pasaporte</option>
+                <option value="pep">Permiso especial de Permanencia</option>
               </select>
             </div>
             <div className={NuevoStyle.inputs}>
               <label htmlFor="id_usuario">Numero de ID:</label>
-              <input type="number" name="id_usuario" value={formData.id_usuario} onChange={handleChange}/>
+              <input type="number" name="id_usuario" value={formData.id_usuario} onChange={handleChange} required/>
             </div>
             <div className={NuevoStyle.select}>
               <label htmlFor="plan">Plan:</label>
-              <select name="plan" value={formData.plan} onChange={handleChange}>
-                <option value="">Mensual - $100</option>
-                <option value="">Semanal - $10</option>
-                <option value="">Anual - $1000</option>
-                <option value="">Diario - $1</option>
-                <option value="">15 dias - $75</option>
-                <option value="">21 dias - $90</option>
+              <select name="plan" value={formData.plan} onChange={handleChange} required>
+                <option value="">Seleccione su plan</option>
+                <option value="A">Anual - $1000</option>
+                <option value="T">Trimestral - $300</option>
+                <option value="M">Mensual - $100</option>
+                <option value="S3">Semana x3 - $90</option>
+                <option value="S2">Semana x2 - $75</option>
+                <option value="S">Semanal - $10</option>
+                <option value="D">Diario - $1</option>
+                <option value="O">Personalizado - $??</option>
               </select>
             </div>
             <div className={NuevoStyle.inputs}>
               <label htmlFor="fechaInicio">Fecha de inicio</label>
-              <input type="date" name="fechaInicio" value={formData.fechaInicio} onChange={handleChange}/>
+              <input type="date" name="fechaInicio" value={formData.fechaInicio} onChange={handleChange} required/>
             </div>
             <button type="submit" className={NuevoStyle.registrarNuevo}>
               Registrar nuevo miembro
