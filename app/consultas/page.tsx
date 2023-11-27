11875119a3bdd000e084a83f0a2ce6b54e6d6ba2
SergioRon2 <rondonjr2005@gmail.com>
2023-11-26 23:17:26 -0500

    26/11/2023

--- app/consultas/page.tsx
+++ app/consultas/page.tsx
@@ -4,14 +4,14 @@ export default function Consultas(){
     return <>
         <div className={ConsultaStyle.container}>
             <div className={ConsultaStyle.formulario}>
-                <h1 className={ConsultaStyle.title}>Lista de Asistencias</h1>
+                <h1 className={ConsultaStyle.title}>Consulta de Asistencias</h1>
                 <form action="">
                     <div className={ConsultaStyle.datos}>
                         <label className="mr-2 text-light">Selecciona una fecha:</label>
                         <input type="date" id="fechaSelector" name="fecha" className="form-control" />
                     </div>
                     <div className={ConsultaStyle.datos}>
-                        <label className="mr-2 text-light">ID del Usuario:</label>
+                        <label className="mr-2 text-light">ID del Usuario (<i className={ConsultaStyle.i}>opcional, por si se quiere ser especifico en la busqueda</i>):</label>
                         <input type="text" id="idUsuario" name="id_usuario" className="form-control" placeholder="Ingrese ID" />
                     </div>
                     <a className={ConsultaStyle.consultarAsistencia} href="">Consultar asistencia</a>
