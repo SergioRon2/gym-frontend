6b93203b543be7092c5c032ff8934f522b06a909
SergioRon2 <rondonjr2005@gmail.com>
2024-03-05 23:49:20 -0500

    05/03/2024

--- app/lector/page.tsx
+++ app/lector/page.tsx
@@ -3,9 +3,9 @@ import LoadingSpinner from '@/components/loading';
 import { apiRestPost } from '@/services/services';
 import { useEffect, useState } from 'react';
 import LectorStyle from 'styles/lector.module.css'
+import swal from 'sweetalert'
 
 export default function Lector(){
-
 
     const [loading, setLoading] = useState(true);
 
@@ -16,22 +16,37 @@ export default function Lector(){
 
 
     const handleFileUpload = async (event:any) => {
-        setLoading(true); // Establece el estado de carga a true mientras se procesa la imagen
-
+        setLoading(true);
         try {
             const formData = new FormData();
             formData.append('imagen', event.target.files[0]);
-
-            // Realiza la llamada a la API utilizando la función apiRestPost de services.ts
+    
             const response = await apiRestPost('/lector/', formData);
-
-            // Maneja la respuesta de la API según sea necesario
             console.log(response);
-
+    
+            // Muestra un SweetAlert dependiendo de la respuesta de la API
+            if (response && response.success) {
+                swal({
+                    title: 'Éxito',
+                    text: 'Imagen procesada correctamente.',
+                    icon: 'success',
+                });
+            } else {
+                swal({
+                    title: 'Error',
+                    text: 'Ocurrió un error al procesar la imagen.',
+                    icon: 'error',
+                });
+            }
         } catch (error) {
             console.error('Error:', error);
+            swal({
+                title: 'Error',
+                text: 'Ocurrió un error al subir la imagen.',
+                icon: 'error',
+            });
         } finally {
-            setLoading(false); // Establece el estado de carga a false después de procesar la imagen
+            setLoading(false); 
         }
     };
 
@@ -43,8 +58,8 @@ export default function Lector(){
                     <h1 className={LectorStyle.title}>Scan QR para asistencias</h1>
                     <div className={LectorStyle.container}>
                         <div className={LectorStyle.containerLector}>
-                            <input type="file" name="" id="" />
-                            <a className={LectorStyle.registrarAsistencia} href={"/consultas"}>Registrar asistencia</a>
+                            <input type="file" name="imagen" id="" onChange={handleFileUpload} />
+                            <a className={LectorStyle.registrarAsistencia}>Registrar asistencia</a>
                         </div>
                     </div>
                 </>
